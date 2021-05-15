a6bb72a1db42f2a80d53d3c541363afecbf471c9
{sunkyuj} <sunkyuj12@gmail.com>
2021-05-15 19:12:16 +0900

    코딩애플 텐서플로우 기초 공부

---
+++ linear regression.py
@@ -0,0 +1,4 @@
+import tensorflow as tf
+
+height = [170,180,175,160]
+weight = [260,270,265,255] 
--- tf.py
+++ tf.py
@@ -1,11 +1,26 @@
 
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+#import tensorflow.compat.v1 as tf
+#tf.disable_v2_behavior()
 
-hello = tf.constant('Hello World!')
-sess = tf.Session()
-print(sess.run(hello))
+import tensorflow as tf
 
+t1 = tf.constant([3,4,5])
+t2 = tf.constant([6,7,8])
+
+t3 = tf.constant([[1,2],[3,4]])
+t4 = tf.constant([[5,6],[7,8]])
+
+t5=tf.zeros([3,3])
+print(t1+t2)
+print(t3*t4)
+print(tf.matmul(t3,t4)) #행렬곱
+print(t5)
+#print(t5.value())
+w = tf.Variable(1.0)
+w.assign(2.7)
+print(w.numpy())
+
+'''
 a=tf.Variable(5)
 b=tf.Variable(3)
 c=tf.multiply(a,b)
@@ -13,5 +28,6 @@ init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 sess.run(c)
+'''
 
-
+ 
